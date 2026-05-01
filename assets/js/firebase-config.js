6aad6fd0df8c8764521d01fc82e119421f0df2f6
imranislam7221-commits <imranislam7221@gmail.com>
2026-05-01 19:26:53 +0000

    Deploying to gh-pages from @ imranislam7221-commits/bdtrippers@1186223e267ef33c470fe0ae01a612e6f826fec8 🚀

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,9 +1,8 @@
-// assets/js/firebase-config.js
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCBdvKSUf0mOUvCA4F338kuxgf1m9i1u3w",
+  authDomain: "bd-trippers.firebaseapp.com",
+  projectId: "bd-trippers",
+  storageBucket: "bd-trippers.firebasestorage.app",
+  messagingSenderId: "972185706940",
+  appId: "1:972185706940:web:cd259421e06658805e0bb9"
 };
