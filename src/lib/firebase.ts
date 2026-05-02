d3a792a08577ba05dddc01b1102ced9228c5aa15
BD Trippers Dev <bd.trippers.dev@gmail.com>
2026-05-03 02:11:30 +0600

    Fix: Change storageBucket URL to appspot.com for better connection compatibility

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCBdvKSUf0mOUvCA4F338kuxgf1m9i1u3w",
   authDomain: "bd-trippers.firebaseapp.com",
   projectId: "bd-trippers",
-  storageBucket: "bd-trippers.firebasestorage.app",
+  storageBucket: "bd-trippers.appspot.com",
   messagingSenderId: "972185706940",
   appId: "1:972185706940:web:cd259421e06658805e0bb9"
 };
